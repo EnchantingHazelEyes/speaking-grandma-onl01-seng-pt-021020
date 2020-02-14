96cb6b04a0aff21cf7bdce45e033050ca1cda188
Samantha Barros <samantha.barros95@gmail.com>
2020-02-14 01:03:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+  phrase = "I LOVE YOU GRANDMA!"
   if phrase == "I LOVE YOU GRANDMA!"
      puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
