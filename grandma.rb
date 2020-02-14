cf1038e7a70bc407ef396c7c0808599a445f5c1c
Samantha Barros <samantha.barros95@gmail.com>
2020-02-14 00:44:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    print "NO, NOT SINCE 1938!"
+  if phrase == phrase.downcase
+     print "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     print "I LOVE YOU TOO PUMPKIN!"
+     print "NO, NOT SINCE 1938!"
   else 
-    print "HUH?! SPEAK UP, SONNY!"
   end
 end  
   
